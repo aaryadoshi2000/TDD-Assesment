eca6d53f356f9c9857b625fe051b93059395dfef
aaryadoshi2000 <aaryamayurpalsingh.doshi@ontariotechu.net>
2024-06-18 02:16:52 -0400

    Update add function to support custom delimiters

--- src/StringCalculator.ts
+++ src/StringCalculator.ts
@@ -3,7 +3,14 @@ export const add = (numbers: string): number => {
     if (numbers === '') {
       return 0;
     }
-    const nums = numbers.split(/,|\n/).map(num => parseInt(num));
+
+    let delimiter = /,|\n/;
+    if (numbers.startsWith('//')) {
+      const parts = numbers.split('\n');
+      delimiter = new RegExp(parts[0].substring(2));
+      numbers = parts[1];
+    }
+
+    const nums = numbers.split(delimiter).map(num => parseInt(num));
     return nums.reduce((sum, num) => sum + num, 0);
   };
-  
