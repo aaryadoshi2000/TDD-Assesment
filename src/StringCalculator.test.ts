8d12e501bddc48c0c89264815f39dd6acc89fb2a
aaryadoshi2000 <aaryamayurpalsingh.doshi@ontariotechu.net>
2024-06-18 02:15:47 -0400

    Add test for supporting custom delimiters

--- src/StringCalculator.test.ts
+++ src/StringCalculator.test.ts
@@ -21,4 +21,9 @@ describe('String Calculator', () => {
     it('should handle new lines as delimiters', () => {
         expect(add('1\n2,3')).toBe(6);
       });
+
+    it('should support custom delimiters', () => {
+        expect(add('//;\n1;2')).toBe(3);
+    });
+  
 });
