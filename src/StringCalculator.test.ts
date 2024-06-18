3001b37229e15fc7ccc94b15a0a33a3361790130
aaryadoshi2000 <aaryamayurpalsingh.doshi@ontariotechu.net>
2024-06-18 02:08:31 -0400

    Add test for the sum of two numbers

--- src/StringCalculator.test.ts
+++ src/StringCalculator.test.ts
@@ -6,9 +6,12 @@ describe('String Calculator', () => {
         expect(add('')).toBe(0);
     });
 
-
     it('should return the number for a single number string', () => {
         expect(add('1')).toBe(1);
+    });
+
+    it('should return the sum of two numbers', () => {
+        expect(add('1,5')).toBe(6);
     });
 
 });
